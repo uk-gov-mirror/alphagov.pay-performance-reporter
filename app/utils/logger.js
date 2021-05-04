83ff0915883cac3c4c1abb5dde32f9cdfa5be46c
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2021-05-04 09:42:35 +0100

    PP-8030 refactor logging
    - refactor logging to use winston so we can take advantage of logstream.

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,10 +1,21 @@
 const { createLogger, format, transports } = require('winston')
 const { json, splat, prettyPrint } = format
+const { govUkPayLoggingFormat } = require('@govuk-pay/pay-js-commons').logging
+const { getLoggingFields } = require('./log-context')
+
+const supplementSharedLoggingFields = format((info) => {
+  if (getLoggingFields()) {
+    return Object.assign(info, getLoggingFields())
+  }
+  return info
+})
 
 const logger = createLogger({
   format: format.combine(
+    supplementSharedLoggingFields(),
     splat(),
     prettyPrint(),
+    govUkPayLoggingFormat({ container: 'performance-reporter', environment: process.env.ENVIRONMENT }),
     json()
   ),
   transports: [
@@ -13,5 +24,5 @@ const logger = createLogger({
 })
 
 module.exports = (loggerName) => {
-  return childLogger = logger.child({ logger_name: loggerName })
+  return logger.child({ logger_name: loggerName })
 }
--- app/utils/request-logger.js
+++ app/utils/request-logger.js
@@ -1,20 +1,44 @@
-'use strict'
+const logger = require('./logger')(__filename)
 
 module.exports = {
   logRequestStart: context => {
-    console.log(`Calling ${context.service} to ${context.description}`, context)
+    logger.info(`Calling ${context.service} to ${context.description}`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description
+    })
   },
 
   logRequestEnd: (context, response) => {
     let duration = new Date() - context.startTime
-    console.log(`${context.method} to ${context.url} ended - elapsed time: ${duration} ms`, context)
+    logger.info(`${context.method} to ${context.url} ended - elapsed time: ${duration} ms`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      response_time: duration,
+      status: response && response.statusCode
+    })
   },
 
   logRequestFailure: (context, response) => {
-    console.log(`Calling ${context.service} to ${context.description} failed`, context, response.statusCode)
+    logger.info(`Calling ${context.service} to ${context.description} failed`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      status: response.statusCode
+    })
   },
 
   logRequestError: (context, error) => {
-    console.log(`Calling ${context.service} to ${context.description} threw exception`, context, error)
+    logger.error(`Calling ${context.service} to ${context.description} threw exception`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      error: error
+    })
   }
 }
